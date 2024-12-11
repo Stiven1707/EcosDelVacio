8c354f42cdac9574a14653386efb63c20ddc6d39
kjimenez <kevin.stiven.jimenez@gmail.com>
2024-12-11 01:38:33 -0500

    con escape se puede volver al menú, no se puede continuar la partida

---
+++ Assets/Scripts/Menu/SCR_Volver_Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SlimUI.ModernMenu
+{
+    public class VolverMenu : MonoBehaviour
+    {
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
+
