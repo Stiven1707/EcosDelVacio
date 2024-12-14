ffad57cf3b33c5cc3d85b4aaa40cedaad65c4f75
kjimenez <kevin.stiven.jimenez@gmail.com>
2024-12-14 00:00:07 -0500

    ahora no se puede usar escape para salir del menu

--- Assets/Scripts/Menu/GameManager.cs
+++ Assets/Scripts/Menu/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !IsMenuSceneLoaded())
         {
             TogglePause();
         }
